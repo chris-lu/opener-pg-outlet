1673a6fdca3a2611836e5d5e83b6f7aac5f66e52
Wilco van Duinkerken <wilco@sparkboxx.com>
2014-03-11 10:01:51 +0100

    add kernel functionality to outlet

--- lib/opener/outlet.rb
+++ lib/opener/outlet.rb
@@ -4,5 +4,11 @@ require_relative 'outlet/server'
 
 module Opener
   class Outlet
+    def run(input, uuid)
+      output = Output.new(:uuid=>uuid, :text=>input)
+      output.save
+
+      return input
+    end
   end
 end

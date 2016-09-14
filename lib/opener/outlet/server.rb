b0b0debdb94421bebb858cc0609e3c0b430758bf
Ubuntu <christophe.dri@gmail.com>
2016-09-14 23:38:35 +0200

    WRK-6: Adding support for error logging

--- lib/opener/outlet/server.rb
+++ lib/opener/outlet/server.rb
@@ -13,6 +13,22 @@ module Opener
           erb :index
         end
       end
+      
+      post '/errors/' do
+        if params[:request_id]
+          output = Output.find_by_uuid(params[:request_id])
+          if output
+            output.errors = params[:error]
+            output.save!
+          else
+            output = Output.new(:uuid=>params[:request_id], :errors=>params[:error])
+            output.uuid = params[:request_id]
+            output.save!
+          end
+        else
+          halt(400, "Bad request: missing request id")
+        end
+      end
 
       get '/:request_id.json' do
         unless params[:request_id] == 'favicon.ico'
@@ -67,3 +83,4 @@ module Opener
     end # Server
   end # Outlet
 end # Opener
+
